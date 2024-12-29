ac57a08fd926895011e9b88e6cb2d9bda3a5c4f9
PectylsonLinho <pectylson@gmail.com>
2024-12-29 19:29:11 +0100

    Retificando o nome da função de teste.

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ from fastapi.testclient import TestClient
 from fast_zero.app import app
 
 
-def test_root_deve_retormar_oke_e_ola_mundo():
+def test_root_deve_retormar_ok_e_ola_mundo():
     client = TestClient(app)
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
